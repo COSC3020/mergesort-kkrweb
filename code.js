5a6d03c6bc84dab0986ad890ebd0206c69ae8152
Kane Kriz <kanemkriz@gmail.com>
2025-01-30 16:31:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
-function mergesort(array) {
+// Kane Kriz
+// UWYO COSC 3020
+// 30 January 2025
+
+
+//iterative and in place version of mergesort
+function mergesort(array)
+{
     return array;
 }
