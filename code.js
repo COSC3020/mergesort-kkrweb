242cde23eff70982bcb2f133cc1efbd97781baf3
Kane Kriz <kanemkriz@gmail.com>
2025-02-03 11:00:16 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,21 @@
 // Kane Kriz
 // UWYO COSC 3020
-// 30 January 2025
-
+// 3 Feb 2025
+//
 
 //iterative and in place version of mergesort
 function mergesort(array)
 {
+    var arrSize = array.length;
+    var splitPoint;
+    var leftSubArrPos;
+    var rightSubArrPos;
+    
+    for()
+        {
+        }
+        
+    //some alteration made to positions of leftSubArrPos and rightSubArrPos to follow mergesort iteratively and properly
+    
     return array;
 }
